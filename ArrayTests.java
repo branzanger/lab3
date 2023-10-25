98429adbacd0e1e75a132bf5aa22dda50b6c1446
Bran Zanger <brandon_z_zhang@Yahoo.com>
2023-10-25 14:21:39 -0700

    Added new test

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,33 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
+
+
+  @Test
+  public void emptyArray(){
+    int[] input = new int[0];
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(input, input);
+  }
+
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    int[] input = { 3 };
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{ 3 }, input);
 	}
 
 
   @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  public void testReversedEven() {
+    int[] input = {1, 2, 3, 4};
+    int[] output = {4, 3, 2, 1};
+    assertArrayEquals(output, ArrayExamples.reversed(input));
+  }
+  @Test
+  public void testReversedOdd() {
+    int[] input = {1, 2, 3, 4, 5};
+    int[] output = {5, 4, 3, 2, 1};
+    assertArrayEquals(output, ArrayExamples.reversed(input));
   }
 }
